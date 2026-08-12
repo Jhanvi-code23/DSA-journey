a14ec3d6648844c67204771230052beaaffb41ac
Jhanvi <jhanvigupta892@gmail.com>
2026-08-12 23:15:31 +0530

    added

--- 06. Arrays/MajorityElement.java
+++ 06. Arrays/MajorityElement.java
@@ -20,25 +20,51 @@ public class MajorityElement {
 
 
     //Better approach- using HashMap
-    public static int majorityElement(int[] nums) {
-        int n = nums.length;
-        java.util.HashMap<Integer, Integer> countMap = new java.util.HashMap<>();
+    // public static int majorityElement(int[] nums) {
+    //     int n = nums.length;
+    //     java.util.HashMap<Integer, Integer> countMap = new java.util.HashMap<>();
         
-        for (int i = 0; i < n; i++) {
-            countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
-            if (countMap.get(nums[i]) > n / 2) {
-                return nums[i]; // Return the majority element
-            }
-        }
+    //     for (int i = 0; i < n; i++) {
+    //         countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
+    //         if (countMap.get(nums[i]) > n / 2) {
+    //             return nums[i]; // Return the majority element
+    //         }
+    //     }
         
-        return -1; // No majority element found
-    }
+    //     return -1; // No majority element found
+    // }
 
 
     
+    //Moore's Voting Algorithm
+    public static int majorityElementMoore(int[] nums) {
+        int count = 0;
+        int el=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                count=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        
+        for (int j=0;j<nums.length;j++){
+            if(nums[j]==el){
+                count++;
+                if(count>nums.length/2){
+                    return el;
+                }
+            }
+        }}
+        return -1;
+    }
     public static void main(String[] args) {
         int[] nums = {3, 2, 3};
-        int result = majorityElement(nums);
+        int result = majorityElementMoore(nums);
         if (result != -1) {
             System.out.println("The majority element is: " + result);
         } else {

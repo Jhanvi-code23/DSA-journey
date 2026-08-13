f14100fe088150c74be877d5e5ab4f88649a45c2
Jhanvi <jhanvigupta892@gmail.com>
2026-08-13 23:10:33 +0530

    added

---
+++ 06. Arrays/StockBuySell.java
@@ -0,0 +1,20 @@
+public class StockBuySell {
+    public static int stock(int[] arr){
+        int minimum=arr[0];
+        int  profit=0;
+
+        for(int i=1;i<arr.length;i++){
+            int cost= arr[i]-minimum;
+            profit=Math.max(profit,cost);
+            minimum=Math.min(minimum,arr[i]);
+        }
+
+        return profit;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr={7,1,5,3,6,4};
+        System.out.println(stock(arr));
+    }
+}
